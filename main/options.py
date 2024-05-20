774371485fb7d2bf736dfe1d1f5e0f989b9e4be8
Liangqi Yuan <69749856+liangqiyuan@users.noreply.github.com>
2024-05-20 03:26:16 -0400

    Update options.py

--- main/options.py
+++ main/options.py
@@ -2,11 +2,10 @@ import argparse
 
 def args_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--framework', type=str, default = 'FedAvg')
+    parser.add_argument('--framework', type=str, default = 'CFL_CD')
     parser.add_argument('--dataset', type=str, default='MNIST')
-    parser.add_argument('--task', type=str, default = 'Class_NonIID')
+    parser.add_argument('--task', type=str, default = 'Class_IID')
     parser.add_argument('--device', type=str, default = 'cuda:0')
-    parser.add_argument('--pretrain', type=str, default = 'False')
     parser.add_argument('--synthetic_number', type=int, default = 600)
     parser.add_argument('--client_number', type=int, default = 20)
     args = parser.parse_args()
